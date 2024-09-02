848a2771a37d8a6f9d2582b25f499ae4a80c2be8
rjindael <me@rjindael.xyz>
2024-09-02 00:44:22 -0700

    feat: periwinkle user lookup

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 # periwinkle
 
-Discord chat server management bot
+Discord management bot for Kiseki instances
 
 Requires:
 
 - discord.py
 - python-dotenv
+
+## Usage
+
+Run `python periwinkle.py` to start the bot
+
+Create a `.env` file with the following contents:
+
+```
+TOKEN=<your discord bot token>
+KISEKI_URL=<the root to your kiseki instance, e.g. http://kiseki.loc>
+API_KEY=<your kiseki API key that you generated with "sail artisan generate-periwinkle-key" (see https://github.com/kiseki-lol/web/trunk/docs/api.md#periwinkle-api-key-generation)>
+```
 
 ## License
 
--- periwinkle.py
+++ periwinkle.py
@@ -1,24 +1,51 @@
 import os
+import time
 import discord
+import requests
+from discord import app_commands
+from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import datetime, timezone
 
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
-
-client = discord.Client(intents=intents)
+client = commands.Bot(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'Logged in as "{client.user}" (ID: {client.user.id})')
+    await client.tree.sync()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.tree.command(name="lookup", description="Look up a Kiseki user by their username or ID")
+async def lookup(interaction: discord.Interaction, query: str):
+    await interaction.response.defer()
 
-    if message.content.startswith('Hi periwinkle'):
-        await message.channel.send('Wazzapppp')
+    url = f"{os.getenv('KISEKI_URL')}/periwinkle/user/lookup"
+    headers = { "Authorization": os.getenv('API_KEY') }
+    params = { "query": query }
+
+    response = requests.post(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        join_date_unix = data.get("created_at")
+        join_date = datetime.fromtimestamp(join_date_unix, tz=timezone.utc)
+        join_date_human = discord.utils.format_dt(join_date, style='R')
+
+        embed = discord.Embed(
+            title=data.get("name"),
+            url=f"{os.getenv('KISEKI_URL')}/user/profile",
+            color=0x8540ff
+        )
+        embed.add_field(name="Currency", value=data.get("currency"), inline=False)
+        embed.add_field(name="Account age", value=join_date_human, inline=False)
+
+        await interaction.followup.send(embed=embed)
+
+    else:
+        await interaction.followup.send(f"failed to lookup user: {response.error}")
 
 client.run(os.getenv('TOKEN'))
