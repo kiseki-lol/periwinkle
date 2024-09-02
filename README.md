7f38ab2f1cb2287e1d8b4b7067de807801e26912
rjindael <me@rjindael.xyz>
2024-09-01 21:24:34 -0700

    feat: initial project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # periwinkle
+
 Discord chat server management bot
+
+Requires:
+
+- discord.py
+- python-dotenv
+
+## License
+
+periwinkle is licensed under the [AGPLv3 license](https://github.com/rjindael/ca-dmv-bot/blob/trunk/LICENSE.md). A copy of it has been included with periwinkle.
---
+++ periwinkle.py
@@ -0,0 +1,24 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('Hi periwinkle'):
+        await message.channel.send('Wazzapppp')
+
+client.run(os.getenv('TOKEN'))
